440d45e8dbafc889b5e640791518f01fe65b1be1
Zatt Wine <tomclick.org@gmail.com>
2021-06-11 19:58:27 +0630

    config env variable exist or not

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/config")();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
