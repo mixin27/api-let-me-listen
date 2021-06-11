310e9d53aa21938e1679d7803bdb4020124e9d27
Zatt Wine <tomclick.org@gmail.com>
2021-06-11 19:57:31 +0630

    config database

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 require("./startup/logging")();
 require("./startup/routes")(app);
+require("./startup/db")();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
