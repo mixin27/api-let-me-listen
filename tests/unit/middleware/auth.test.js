0ab7216da97db8481b1bfd31ed1413c52543f147
Zatt Wine <tomclick.org@gmail.com>
2021-06-11 20:24:18 +0630

    add auth middleware unit test functionality

--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -1,5 +1,24 @@
+const mongoose = require("mongoose");
+
+const { User } = require("../../../models/user");
 const auth = require("../../../middleware/auth");
 
 describe("auth middleware", () => {
-  it("should return 401 if token is not provided", () => {});
+  it("should populate req.user with the payload of a valid JWT", () => {
+    const user = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+    const token = new User(user).generateAuthToken();
+
+    const req = {
+      header: jest.fn().mockReturnValue(token),
+    };
+    const res = {};
+    const next = jest.fn();
+
+    auth(req, res, next);
+
+    expect(req.user).toMatchObject(user);
+  });
 });
